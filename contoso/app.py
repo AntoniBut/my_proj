88a7649fc6837badb08ab6a865b9bc374752873a
antonida <antonida.b@gmail.com>
2022-11-28 19:26:39 +0300

    first commit

--- contoso/app.py
+++ contoso/app.py
@@ -1,10 +1,9 @@
 import flask
 from flask import render_template
 import pickle
-import sklearn
-from sklearn.neighbors import KNeighborsRegressor
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import Normalizer
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -15,8 +14,8 @@ def main():
     if flask.request.method=="GET":
         return render_template('main.html')
     if flask.request.method=="POST":
-        with open('C:/Users/Antonida.Butuzova/my_proj/contoso/KNNreg_pkl.pkl','rb') as f:
-            loaded_model=pickle.load(f)
+        loaded_model = pickle.load(open('contoso/reg_pkl.pkl', 'rb'))
+                
         IW=float(flask.request.form['IW'])
         IF=float(flask.request.form['IF'])
         VW=float(flask.request.form['VW'])
@@ -24,10 +23,12 @@ def main():
         
         a = np.array([[IW,IF,VW,FP]])
         X_pred=pd.DataFrame(a)
-        
+        norm = Normalizer()
+        X_pred = norm.fit_transform(X_pred)      
+        print(X_pred)  
         y_pred=loaded_model.predict(X_pred)
-        str_res='Глубина шва (Depth) = '+str(y_pred[0,0])+' и ширина шва (Width)='+str(y_pred[0,1])
-        print(str_res)
+        str_res='Глубина шва (Depth) = '+str(round(y_pred[0,0],2))+' и ширина шва (Width)='+str(round(y_pred[0,1],2))
+        print(X_pred)
         return render_template('main.html',result=str_res)
 if __name__=='__main__':
     app.run()
